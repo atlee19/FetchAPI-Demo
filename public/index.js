5d75d31055557849a21dd1ce7226c97642e9646f
atlee19 <atleegraham16@gmail.com>
2020-10-12 20:40:34 -0400

    get single user data working

--- public/index.js
+++ public/index.js
@@ -1,4 +1,16 @@
 //this code will manipulate DOM
 import Data from './fetch.js'
 
-console.log(Data.getSingleUser())
+const Load = {
+
+    SingleUserData : function(){
+        const response = Data.getSingleUser()
+        response.then(res => res.json())
+        .then(data => {
+            const user = data.data 
+            console.log(user.id)
+        })
+    }
+}
+
+Load.SingleUserData()
